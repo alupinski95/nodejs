e48a9f2ee1363b14b69199cdc99c0bc22039e50a
alupinski <lbpro.arkadiusz.lupinksi@gmail.com>
2022-05-30 15:09:11 +0200

    Image 404

--- SEM2/Exam/app.js
+++ SEM2/Exam/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const status = require("status");
+const path = require('path');
 const app = express();
 const { errorMiddleware } = require("./errorLog");
 const { authMiddleware } = require("./authMiddleware");
@@ -9,7 +10,7 @@ const { init } = require("./mongoDBCliend");
 
 init().then(() => {
 	app.use(errorMiddleware);
-	app.use(authMiddleware);
+	//app.use(authMiddleware);
 
 
 
@@ -18,8 +19,13 @@ init().then(() => {
 		res.send(new Date());
 	});
 	app.all("*", (req, res) => {
-		res.sendFile('');
+		res.sendFile(path.join(__dirname,'/shared/404error.html'));
 	});
 });
+
+app.use(express.static('public')); 
+app.use('/images', express.static('images'));
+// app.use(express.static(__dirname + '/images'));
+
 
 app.listen(process.env.PORT, () => console.log("Server Started"));
